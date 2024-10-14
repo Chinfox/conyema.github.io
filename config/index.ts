279fb365a8a62594ace3bd9537f1f7fa474503e8
Chinedum Onyema <onyemachinedum@gmail.com>
2024-10-14 21:36:07 +0100

    fix(FIA link): Change API link

--- config/index.ts
+++ config/index.ts
@@ -2,6 +2,8 @@
 const dev = process.env.NODE_ENV === "development";
 // console.log("environment: ", dev);
 
-const apiServer = dev ? "http://localhost:4000/api" : "https://owl-hek7.onrender.com/api";
+// const apiServer = dev ? "http://localhost:4000/api" : "https://owl-hek7.onrender.com/api";
+const apiServer = dev ? "http://localhost:4000/api" : "https://predict-financial-inclusion-africa.onrender.com";
+
 
 export default apiServer;
